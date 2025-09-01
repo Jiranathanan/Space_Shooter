23e9a3349f728672a67be1d867ddff803b8b4e58
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-09-01 21:25:56 +0700

    VisibilityNotifier & screen_exited

--- Enemy.gd
+++ Enemy.gd
@@ -9,5 +9,11 @@ func _process(delta):
 
 func _on_Enemy_body_entered(body):
 	body.queue_free()
-	queue_free()
+	ARMOR -= 1
+	if ARMOR <= 0:
+		queue_free()
 	
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
